51d14d28d06a77bdc001952cd66f270a57619e0a
Adam Drake <adam@chemify.io>
2022-11-18 16:00:29 +0100

    update

--- src/components/SEOComponent.tsx
+++ src/components/SEOComponent.tsx
@@ -14,29 +14,32 @@ export const SEOComponent = ({
   siteUrl,
   imageUrl,
 }: SEOComponentProps) => {
+  const image =
+    imageUrl ||
+    "https://res.cloudinary.com/dmiizmobu/image/upload/v1668758334/openai-beauty/DALL_E_2022-11-18_08.57.24.png";
+
   return (
     <>
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@FrontEndDrake" />
-      <meta name="twitter:creator" content="@FrontEndDrake" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:url" content={siteUrl || SITE_URL} />
       <meta property="og:type" content="website" />
-      <meta
-        property="og:image"
-        content={
-          imageUrl ||
-          "https://res.cloudinary.com/dmiizmobu/image/upload/v1668758334/openai-beauty/DALL_E_2022-11-18_08.57.24.png"
-        }
-      />
-      <meta property="og:image:alt" content="" />
+      <meta property="og:image" content={image} />
+      <meta property="og:image:alt" content={title} />
       <meta property="og:image:width" content="1600" />
       <meta property="og:image:height" content="900" />
       <meta property="og:locale" content="en_IE" />
       <meta property="og:site_name" content={SITE_NAME} />
       <meta name="robots" content="index,follow" />
       <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@FrontEndDrake" />
+      <meta name="twitter:creator" content="@FrontEndDrake" />
+      <meta name="twitter:domain" content={SITE_URL} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={image} />
+      <meta name="twitter:url" content={siteUrl || SITE_URL} />
     </>
   );
 };
