99fe8848737cd73490f757249ec070be45b7e93c
Adam Drake <adamgedrake@gmail.com>
2022-11-18 09:26:24 +0100

    adding some SEO basics (#8)
    Co-authored-by: Adam Drake <adam@chemify.io>

--- TODO.md
+++ TODO.md
@@ -1,10 +1,8 @@
 # Todos
 
-- Render params passed through to make poem
 - lazy loading for poems more than 9
 - Fonts for Headings
 - Favicon
-- SEO
 - Google Analytics
 - One landing page about poems
 - Social links to my github and homepage
--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
-export const SITE_NAME = "AI Beauty";
+export const SITE_NAME = "OpenAI GPT-3 Poems";
+
+export const SITE_URL = "https://openai-beauty.vercel.app";
 
 export const API_URL_ENDPOINT =
-  process.env.NODE_ENV === "production"
-    ? "https://openai-beauty.vercel.app"
-    : "http://localhost:3000";
+  process.env.NODE_ENV === "production" ? SITE_URL : "http://localhost:3000";
