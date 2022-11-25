f9c34ebae46ce610405883da5a70131cf8e3f253
Adam Drake <adam@chemify.io>
2022-11-25 09:05:48 +0100

    adding analytics

---
+++ src/utils/gtag.ts
@@ -0,0 +1,26 @@
+export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  // @ts-ignore
+  window.gtag("config", GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+type EventArgs = {
+  action: string;
+  category: string;
+  label: string;
+  value: string;
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({ action, category, label, value }: EventArgs) => {
+  // @ts-ignore
+  window.gtag("event", action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
