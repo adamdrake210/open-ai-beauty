a0b9fc4b28001221cb0bb95414ebed8e8923c664
Adam Drake <adam@chemify.io>
2022-12-21 10:36:39 +0100

    updating frontend

--- frontend/src/pages/admin-create.tsx
+++ frontend/src/pages/admin-create.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 import Head from "next/head";
+import { GetServerSideProps } from "next";
 
 import Layout from "@/layout/Layout";
 import { CreatePoemForm } from "@/components/create/CreatePoemForm";
 import { SITE_ICON } from "@/constants/constants";
+import { parseCookies } from "@/utils/cookies";
+import isEmpty from "lodash.isempty";
 
-export default function Home() {
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const data = parseCookies(req);
+  console.log(
+    "🚀 ~ file: admin-create.tsx:13 ~ constgetServerSideProps:GetServerSideProps= ~ data",
+    data
+  );
+
+  if (isEmpty(data)) {
+    res.statusCode = 403;
+    return {
+      redirect: {
+        destination: "/login",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function AdminArea() {
   return (
     <>
       <Head>
