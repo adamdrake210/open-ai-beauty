cac6146df7e896b8c5d8d544681a99e9e8d6c38e
Adam Drake <adam@chemify.io>
2022-12-27 16:25:24 +0100

    update frontend

--- frontend/src/utils/tokens.ts
+++ frontend/src/utils/tokens.ts
@@ -5,12 +5,12 @@ import { IncomingMessage } from "http";
 export async function userFromRequest(
   req: IncomingMessage & { cookies: NextApiRequestCookies }
 ): Promise<any> {
-  const { Refresh: token } = req.cookies;
+  const { Authentication: token } = req.cookies;
 
   if (!token) return undefined;
 
   try {
-    const data = jwt.verify(token, process.env.REFRESH_JWT_TOKEN_KEY!);
+    const data = jwt.verify(token, process.env.JWT_TOKEN_KEY!);
 
     if (!data) return undefined;
 
