bef172110a58bd93a218591b878065e1fa7d0942
Adam Drake <adam@chemify.io>
2023-01-04 18:05:09 +0100

    clean up

--- frontend/src/styles/theme.ts
+++ frontend/src/styles/theme.ts
@@ -2,12 +2,6 @@ import { MantineThemeOverride } from "@mantine/core";
 
 export const theme: MantineThemeOverride = {
   colorScheme: "light",
-  colors: {
-    // Add your color
-    deepBlue: ["#E9EDFC", "#C1CCF6", "#99ABF0" /* ... */],
-    // or replace default theme color
-    blue: ["#E9EDFC", "#C1CCF6", "#99ABF0" /* ... */],
-  },
 
   shadows: {
     md: "1px 1px 3px rgba(0, 0, 0, .25)",
