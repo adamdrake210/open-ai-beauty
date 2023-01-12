e4d98ac5ff44b8d742babeecf69a4dbe78ad301d
Adam Drake <adam@chemify.io>
2023-01-12 14:09:10 +0100

    update cookies

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -115,7 +115,7 @@ export class AuthService {
       '🚀 ~ file: auth.service.ts:114 ~ AuthService ~ getCookieWithJwtToken ~ accessToken',
       accessToken
     );
-    return `Authentication=${accessToken}; HttpOnly; Secure; Path=/; Max-Age=${this.jwtConfiguration.accessTokenTtl}`;
+    return `Authentication=${accessToken}; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=${this.jwtConfiguration.accessTokenTtl}`;
   }
 
   public async getCookieWithJwtRefreshToken(userId: string) {
@@ -125,7 +125,7 @@ export class AuthService {
       this.jwtConfiguration.refreshTokenTtl,
       payload
     );
-    const cookie = `Refresh=${refreshToken}; HttpOnly; Secure; Path=/; Max-Age=${this.jwtConfiguration.refreshTokenTtl}`;
+    const cookie = `Refresh=${refreshToken}; HttpOnly; Secure; SameSite=None; Path=/; Max-Age=${this.jwtConfiguration.refreshTokenTtl}`;
     return {
       cookie,
       token: refreshToken,
