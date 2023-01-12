d6a591ea973165bae17bc08254f3217fd1acb47d
Adam Drake <adam@chemify.io>
2023-01-12 15:17:36 +0100

    maybe...

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
     app.enableCors({
       origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
       credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      // methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     });
   }
 
