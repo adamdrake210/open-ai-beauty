3176814ec5b88270bb2388aaa71a9338bb808983
Adam Drake <adam@chemify.io>
2023-01-23 08:59:11 +0100

    basic validation on register form

--- frontend/src/components/auth/RegisterForm.tsx
+++ frontend/src/components/auth/RegisterForm.tsx
@@ -6,10 +6,11 @@ import { Loader } from "../common/Loader";
 import { handleUnknownError } from "@/utils/handleUnknownError";
 import { useRouter } from "next/router";
 import { CREATE_POST } from "@/constants/routeConstants";
-import { Box, Button, Center, Input } from "@mantine/core";
+import { Box, Button, Center, Flex, Input, Title } from "@mantine/core";
 import { ErrorMessage } from "../ErrorMessage";
 
 export const RegisterForm = () => {
+  const [passwordError, setPasswordError] = React.useState("");
   const router = useRouter();
 
   const { mutate, isLoading, isError, error } = useMutation({
@@ -44,16 +45,24 @@ export const RegisterForm = () => {
     lastname: string | null;
     email: string;
     password: string;
+    confirmpassword?: string;
   }>({
     defaultValues: {
       firstname: null,
       lastname: null,
       email: "",
       password: "",
+      confirmpassword: "",
     },
   });
 
   const onSubmit = handleSubmit((data) => {
+    setPasswordError("");
+
+    if (data.password !== data.confirmpassword) {
+      setPasswordError("Passwords do not match. Please re-enter passwords.");
+      return;
+    }
     try {
       mutate(data, {
         onSuccess: () => {
@@ -67,106 +76,141 @@ export const RegisterForm = () => {
   });
 
   return (
-    <Box
-      component="form"
-      onSubmit={onSubmit}
-      sx={{ width: "100%", maxWidth: "250px", mx: "auto" }}
-    >
-      <Input.Wrapper
-        sx={{ marginBottom: 8 }}
-        id="firstname"
-        withAsterisk
-        label="First Name"
-        error={errors.firstname?.message}
+    <Flex direction="column" align="center" my={32}>
+      <Title order={3} mb={4}>
+        Sign up with Email
+      </Title>
+      <Box
+        component="form"
+        onSubmit={onSubmit}
+        sx={{ width: "250px", mx: "auto" }}
       >
-        <Input
+        <Input.Wrapper
+          sx={{ marginBottom: 8 }}
           id="firstname"
-          disabled={isLoading}
-          {...register("firstname", {
-            required: {
-              value: true,
-              message: `Please complete this required field`,
-            },
-          })}
-          placeholder="Enter first name here"
-        />
-      </Input.Wrapper>
-
-      <Input.Wrapper
-        sx={{ marginBottom: 8 }}
-        id="lastname"
-        withAsterisk
-        label="Last Name"
-        error={errors.lastname?.message}
-      >
-        <Input
-          id="lastname"
-          disabled={isLoading}
-          {...register("lastname", {
-            required: {
-              value: true,
-              message: `Please complete this required field`,
-            },
-          })}
-          placeholder="Enter last name here"
-        />
-      </Input.Wrapper>
-
-      <Input.Wrapper
-        sx={{ marginBottom: 8 }}
-        id="email"
-        withAsterisk
-        label="Email"
-        error={errors.email?.message}
-      >
-        <Input
-          id="email"
-          disabled={isLoading}
-          {...register("email", {
-            required: {
-              value: true,
-              message: `Please complete this required field`,
-            },
-          })}
-          placeholder="Enter your email here"
-        />
-      </Input.Wrapper>
-
-      <Input.Wrapper
-        sx={{ marginBottom: 8 }}
-        id="password"
-        withAsterisk
-        label="Password"
-        error={errors.password?.message}
-      >
-        <Input
-          id="password"
-          type="password"
-          disabled={isLoading}
-          {...register("password", {
-            required: {
-              value: true,
-              message: `Please complete this required field`,
-            },
-          })}
-          placeholder="Enter a secure password here"
-        />
-      </Input.Wrapper>
-
-      {isLoading && <Loader loadingText="Registering..." />}
-      <Center>
-        <Button
-          type="submit"
-          color="grape"
-          radius="md"
-          size="lg"
-          mt={16}
-          disabled={isLoading}
+          withAsterisk
+          label="First Name"
+          error={errors.firstname?.message}
         >
-          Sign Up
-        </Button>
-      </Center>
-      {isError && <ErrorMessage>{handleUnknownError(error)}</ErrorMessage>}
-    </Box>
+          <Input
+            id="firstname"
+            disabled={isLoading}
+            {...register("firstname", {
+              required: {
+                value: true,
+                message: `Please complete this required field`,
+              },
+            })}
+            placeholder="Enter first name here"
+          />
+        </Input.Wrapper>
+
+        <Input.Wrapper
+          sx={{ marginBottom: 8 }}
+          id="lastname"
+          withAsterisk
+          label="Last Name"
+          error={errors.lastname?.message}
+        >
+          <Input
+            id="lastname"
+            disabled={isLoading}
+            {...register("lastname", {
+              required: {
+                value: true,
+                message: `Please complete this required field`,
+              },
+            })}
+            placeholder="Enter last name here"
+          />
+        </Input.Wrapper>
+
+        <Input.Wrapper
+          sx={{ marginBottom: 8 }}
+          id="email"
+          withAsterisk
+          label="Email"
+          error={errors.email?.message}
+        >
+          <Input
+            id="email"
+            disabled={isLoading}
+            {...register("email", {
+              required: {
+                value: true,
+                message: `Please complete this required field`,
+              },
+            })}
+            placeholder="Enter your email here"
+          />
+        </Input.Wrapper>
+
+        <Input.Wrapper
+          sx={{ marginBottom: 8 }}
+          id="password"
+          withAsterisk
+          label="Password"
+          error={errors.password?.message}
+        >
+          <Input
+            id="password"
+            type="password"
+            disabled={isLoading}
+            {...register("password", {
+              required: {
+                value: true,
+                message: `Please complete this required field`,
+              },
+              minLength: {
+                value: 8,
+                message: `Password must be at least 8 characters`,
+              },
+            })}
+            placeholder="Enter a secure password here"
+          />
+        </Input.Wrapper>
+
+        <Input.Wrapper
+          sx={{ marginBottom: 8 }}
+          id="confirmpassword"
+          withAsterisk
+          label="Confirm Password"
+          error={errors.password?.message}
+        >
+          <Input
+            id="confirmpassword"
+            type="password"
+            disabled={isLoading}
+            {...register("confirmpassword", {
+              required: {
+                value: true,
+                message: `Please complete this required field`,
+              },
+              minLength: {
+                value: 8,
+                message: `Password must be at least 8 characters`,
+              },
+            })}
+            placeholder="Confirm your password here"
+          />
+        </Input.Wrapper>
+
+        {isLoading && <Loader loadingText="Registering..." />}
+        <Center>
+          <Button
+            type="submit"
+            color="grape"
+            radius="md"
+            size="lg"
+            mt={16}
+            disabled={isLoading}
+          >
+            Sign Up
+          </Button>
+        </Center>
+        {isError && <ErrorMessage>{handleUnknownError(error)}</ErrorMessage>}
+        {passwordError && <ErrorMessage>{passwordError}</ErrorMessage>}
+      </Box>
+    </Flex>
   );
 };
